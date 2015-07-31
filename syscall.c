3862e4c6da9bd75a29c3e07f702ea398310e80f9
Daniele Lacamera <daniele.lacamera@tass.be>
2015-07-31 19:23:04 +0200

    Syscalls no longer allowed from kernel space

--- syscall.c
+++ syscall.c
@@ -4,6 +4,8 @@ volatile int _syscall_retval = 0;
 
 int syscall(uint32_t syscall_nr, uint32_t arg1, uint32_t arg2, uint32_t arg3, uint32_t arg4, uint32_t arg5)
 {
+    if (scheduler_get_cur_pid() == 0)
+        return -1; /* Syscalls not allowed from kernel */
     return _syscall(syscall_nr, arg1, arg2, arg3, arg4, arg5);
 }
 
