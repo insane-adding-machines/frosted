2f7fffb9e45f8f8f78e629b41a7a528ce5e58249
Daniele Lacamera <daniele.lacamera@tass.be>
2015-08-08 13:47:43 +0200

    Stellaris: Added more interrupts

--- port/stellaris/stellaris.c
+++ port/stellaris/stellaris.c
@@ -28,32 +28,6 @@ void SysTick_Handler(void);
 extern int main(void);
 extern unsigned int __StackTop; /* provided by linker script */
  
-//*****************************************************************************
-//
-// The vector table.  Note that the proper constructs must be placed on this to
-// ensure that it ends up at physical address 0x0000.0000.
-//
-//*****************************************************************************
-__attribute__ ((section(".isr_vector")))
-void (* const g_pfnVectors[])(void) =
-{
-    (void *)&__StackTop,                    // The initial stack pointer
-    Reset_Handler,                          // The reset handler
-    NMI_Handler,                            // The NMI handler
-    HardFault_Handler,                      // The hard fault handler
-    MemManage_Handler,                      // The MPU fault handler
-    BusFault_Handler,                       // The bus fault handler
-    UsageFault_Handler,                     // The usage fault handler
-    0,                                      // Reserved
-    0,                                      // Reserved
-    0,                                      // Reserved
-    0,                                      // Reserved
-    SVC_Handler,                            // SVCall handler
-    DebugMon_Handler,                       // Debug monitor handler
-    0,                                      // Reserved
-    PendSV_Handler,                         // The PendSV handler
-    SysTick_Handler                         // The SysTick handler
-};
  
 //*****************************************************************************
 //
@@ -168,3 +142,204 @@ void __attribute__((weak)) IntDefaultHandler(void)
         ;
     }
 }
+
+void __attribute__((weak)) WDT_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) TIMER0_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) TIMER1_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) TIMER2_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) TIMER3_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) UART0_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) UART1_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) UART2_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) UART3_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) PWM1_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) I2C0_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) I2C1_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) I2C2_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) SPI_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) SSP0_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) SSP1_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) PLL0_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) RTC_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) EINT0_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) EINT1_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) EINT2_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) EINT3_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) ADC_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) BOD_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) USB_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) CAN_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) DMA_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) I2S_IRQHandler(void)
+{
+    while(1);
+}
+
+void __attribute__((weak)) ENET_IRQHandler(void)
+{
+    while(1);
+}
+
+//*****************************************************************************
+//
+// The vector table.  Note that the proper constructs must be placed on this to
+// ensure that it ends up at physical address 0x0000.0000.
+//
+//*****************************************************************************
+__attribute__ ((section(".isr_vector")))
+void (* const g_pfnVectors[])(void) =
+{
+    (void *)&__StackTop,                    // The initial stack pointer
+    Reset_Handler,                          // The reset handler
+    NMI_Handler,                            // The NMI handler
+    HardFault_Handler,                      // The hard fault handler
+    MemManage_Handler,                      // The MPU fault handler
+    BusFault_Handler,                       // The bus fault handler
+    UsageFault_Handler,                     // The usage fault handler
+    0,                                      // Reserved
+    0,                                      // Reserved
+    0,                                      // Reserved
+    0,                                      // Reserved
+    SVC_Handler,                            // SVCall handler
+    DebugMon_Handler,                       // Debug monitor handler
+    0,                                      // Reserved
+    PendSV_Handler,                         // The PendSV handler
+    SysTick_Handler,                        // The SysTick handler
+    WDT_IRQHandler,
+    TIMER0_IRQHandler,
+    TIMER1_IRQHandler,
+    TIMER2_IRQHandler,
+    TIMER3_IRQHandler,
+    UART0_IRQHandler,
+    UART1_IRQHandler,
+    UART2_IRQHandler,
+    UART3_IRQHandler,
+    PWM1_IRQHandler,
+    I2C0_IRQHandler,
+    I2C1_IRQHandler,
+    I2C2_IRQHandler,
+    SPI_IRQHandler,
+    SSP0_IRQHandler,
+    SSP1_IRQHandler,
+    PLL0_IRQHandler,
+    RTC_IRQHandler,
+    EINT0_IRQHandler,
+    EINT1_IRQHandler,
+    EINT2_IRQHandler,
+    EINT3_IRQHandler,
+    ADC_IRQHandler,
+    BOD_IRQHandler,
+    USB_IRQHandler,
+    CAN_IRQHandler,
+    DMA_IRQHandler,
+    I2S_IRQHandler,
+    ENET_IRQHandler,
+};
