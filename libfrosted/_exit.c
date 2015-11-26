a5f5b27501b41520037c7fed367e2dbc1ae17ba4
maximevince <maxime.vince@gmail.com>
2015-11-26 18:03:52 +0100

    Add _exit syscall to libfrosted

--- libfrosted/_exit.c
+++ libfrosted/_exit.c
@@ -8,7 +8,12 @@
 
 extern int (**__syscall__)(int rc);
 
-void exit(int rc)
+void __attribute__((weak)) exit(int rc)
+{
+    __syscall__[SYS_EXIT](rc);
+}
+
+void _exit(int rc)
 {
     __syscall__[SYS_EXIT](rc);
 }
