ad6995122a6ee35e99572a6be5757b927dba51ab
Daniele Lacamera <root@danielinux.net>
2015-12-01 15:58:33 +0100

    Added exit() call to library

---
+++ libfrosted/exit.c
@@ -0,0 +1,16 @@
+/*
+ * Frosted version of exit.
+ */
+
+#include "frosted_api.h"
+#include "syscall_table.h"
+#include <errno.h>
+#undef errno
+extern int errno;
+extern int (**__syscall__)(int val);
+
+int exit(int val)
+{
+    return __syscall__[SYS_EXIT](val);
+}
+
