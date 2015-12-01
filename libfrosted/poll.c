533007e4b69a82d4655d19940764fe93adb811c6
Daniele Lacamera <root@danielinux.net>
2015-12-01 15:57:05 +0100

    Added poll library interface.

---
+++ libfrosted/poll.c
@@ -0,0 +1,18 @@
+/*
+ * Frosted version of poll.
+ */
+
+#include "frosted_api.h"
+#include "syscall_table.h"
+#include <errno.h>
+#undef errno
+extern int errno;
+
+struct pollfd;
+
+extern int (**__syscall__)(struct pollfd *pfd, int nfds, int timeout);
+
+int poll(struct pollfd *pfd, int nfds, int timeout)
+{
+    return __syscall__[SYS_POLL](pfd, nfds, timeout);
+}
