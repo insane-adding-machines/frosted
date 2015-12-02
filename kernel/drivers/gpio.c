ff58fea48f72b4d0a8f6cbca74e03dc7c54e3fb9
DarkVegetableMatter <darkvegetablematter@gmail.com>
2015-12-01 20:52:20 -0800

    Fix build error in gpio.c

--- kernel/drivers/gpio.c
+++ kernel/drivers/gpio.c
@@ -5,7 +5,7 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/exti.h>
 #include <libopencm3/stm32/rcc.h>
-#include <libopencm3/cm3/nvic.h>                        <<<<<<<<<<<<<<<<<<<<
+#include <libopencm3/cm3/nvic.h>
 #endif
 #ifdef LPC17XX
 #include <libopencm3/lpc17xx/nvic.h>
