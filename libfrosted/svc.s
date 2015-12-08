f34d1e807d89ca7ffa77fd2ecdfedae08e586b62
maximevince <maxime.vince@gmail.com>
2015-12-08 23:55:55 +0100

    svc that saves and restores r4-r11

--- libfrosted/svc.s
+++ libfrosted/svc.s
@@ -1,8 +1,31 @@
 .syntax unified
 
+.text
+
 .global _syscall
 _syscall:
-    nop
+save_r0:
+    push {r0};
+
+save_context:
+    mrs r0, PSP
+    stmdb r0!, {r4-r11}
+    msr PSP, r0
+
+restore_r0:
+    add r0, r0, #32
+    ldr r0, [r0] 
+
+    isb
     svc 0
-    nop
+
+restore_context:
+    mrs r1, PSP
+    ldmfd r1!, {r4-r11}
+compensate_for_push:
+    add r1,r1, #4
+    msr PSP, r1
+
+    isb
     bx lr
+
