2915780aad3541a59e4a54eb5edcea871fdc9551
Daniele Lacamera <root@danielinux.net>
2016-08-01 16:27:33 +0200

    Changes needed to work with unicore-mx usb-rewrite

--- kernel/drivers/usb.h
+++ kernel/drivers/usb.h
@@ -3,9 +3,10 @@
 #include "frosted.h"
 #include "gpio.h"
 #include <unicore-mx/usbd/usbd.h>
-#include <unicore-mx/usb/cdc.h>
+#include <unicore-mx/usb/class/cdc.h>
 #include <unicore-mx/cm3/scb.h>
 #include <unicore-mx/cm3/nvic.h>
+#include <unicore-mx/usbd/misc/string.h>
 
 #define USB_MODE_GUEST 1
 #define USB_MODE_HOST  2
@@ -15,26 +16,13 @@ struct usb_config {
     struct gpio_config pio_vbus, pio_dm, pio_dp;
 };
 
-
-#define DEVUSB_BUFLEN 128
-#define DEVUSB_STRINGS 128
-struct devusb_config {
-    usbd_device *usbd_dev;
-    const struct usb_device_descriptor *dev_desc;
-    const struct usb_config_descriptor *conf_desc;
-    const struct usb_endpoint_descriptor * data_endp;
-    const char ** strings;
-    int n_strings;
-    uint8_t  buffer[DEVUSB_BUFLEN];
-    void (*callback)(usbd_device *usbd_dev, uint16_t wValue);
-};
-
 #ifdef CONFIG_DEVUSB
     int usb_init(struct usb_config *conf);
-    int usbdev_start(struct devusb_config *conf);
+    int usbdev_start(usbd_device **_usbd_dev,
+          const struct usb_device_descriptor *dev_desc);
 #else
 #  define usb_init(x) ((-ENOENT))
-#  define usbdev_start(x) ((-ENOENT))
+#  define usbdev_start(x,y) ((-ENOENT))
 #endif
 
 
