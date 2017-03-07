c33aa9f524d131e34c5f2cfc83818657a1db44cd
brabo <brabo@cryptolab.net>
2017-03-07 01:41:54 +0100

    updated authors

--- kernel/locks.c
+++ kernel/locks.c
@@ -14,7 +14,7 @@
  *      You should have received a copy of the GNU General Public License
  *      along with frosted.  If not, see <http://www.gnu.org/licenses/>.
  *
- *      Authors: Daniele Lacamera, Maxime Vincent
+ *      Authors: Daniele Lacamera, Maxime Vincent, brabo
  *
  */
 #include "frosted.h"
